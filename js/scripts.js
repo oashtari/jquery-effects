b5af8e6886a8559eb35928b7c8b9ac5e34e4e30f
Michael Munyaneza and Omid Ashtari <michael@>
2016-08-31 20:46:20 -0700

    add jQuery functions to slide lion, fade wombat, body color, fade h4, reset page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(function() {
-  $("h2").fadeIn();
-  $(".lion").fadeIn();
-  $(".wombat").fadeIn();
-
+  $("h2").click(function() {
+    $(".lion").slideUp();
+    $("body").addClass("bodyClass");
+  });
+  $(".wombat").click(function() {
+    $(".wombat").fadeOut();
+    $("body").removeClass();
+  });
+  $("#second").click(function() {
+    $("h4").fadeIn();
+  });
+  $("#right").click(function() {
+    $(".lion").fadeIn();
+    $(".wombat").fadeIn();
+    $("body").removeClass();
   });
 });
-
-// $(function() {
-//   $("#lion").fadeIn(function(){
-//
-//   });
-// });
